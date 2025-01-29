891948b3c13b6a320f4bc855d0d904f1070a6039
Augusto Melo <4723788+augustomelo@users.noreply.github.com>
2025-01-29 10:13:35 +0000

    feat: improve logging on activation

--- users/augusto/activation/vale.nix
+++ users/augusto/activation/vale.nix
@@ -15,6 +15,7 @@ in
     (
       dic_path=${config.xdg.dataHome}/dictionaries
       if [[ ! -d $dic_path ]]; then
+        $DRY_RUN_CMD echo "Creating dictionary folder at: $dic_path"
         $DRY_RUN_CMD mkdir -p $dic_path
       fi
 
@@ -27,6 +28,7 @@ in
       yaml_content=$(echo "$yaml_content" | ${pkgs.dasel}/bin/dasel put --read yaml --type string --value "\"%s\" is a typo!" 'message' )
 
       for lang in ${toString langs}; do
+        $DRY_RUN_CMD echo "Dowlading dictionary: $lang"
         ${pkgs.curl}/bin/curl --silent --show-error --output "$dic_path/$lang.dic" "https://raw.githubusercontent.com/wooorm/dictionaries/refs/heads/main/dictionaries/$lang/index.dic"
         ${pkgs.curl}/bin/curl --silent --show-error --output  "$dic_path/$lang.aff" "https://raw.githubusercontent.com/wooorm/dictionaries/refs/heads/main/dictionaries/$lang/index.aff"
         yaml_content=$(echo "$yaml_content" | ${pkgs.dasel}/bin/dasel put --read yaml --type string --value "$lang" 'dictionaries.[]')
--- users/augusto/activation/zsh.nix
+++ users/augusto/activation/zsh.nix
@@ -2,39 +2,43 @@
 {
   home.activation.zshActivation = lib.hm.dag.entryAfter ["writeBoundary"] ''
     (
-      hist_folder=${config.xdg.stateHome}/zsh
-      if [[ ! -d $hist_folder ]]; then
-        $DRY_RUN_CMD mkdir -p $hist_folder
+      hist_path=${config.xdg.stateHome}/zsh
+      if [[ ! -d $hist_path ]]; then
+        $DRY_RUN_CMD echo "Creating history folder at: $hist_path"
+        $DRY_RUN_CMD mkdir -p $hist_path
       fi
 
-      completion_folder=${config.xdg.dataHome}/zsh/completion
-      if [[ ! -d $completion_folder ]]; then
-        $DRY_RUN_CMD mkdir -p $completion_folder
+      completion_path=${config.xdg.dataHome}/zsh/completion
+      if [[ ! -d $completion_path ]]; then
+        $DRY_RUN_CMD echo "Creating completion folder at: $completion_path"
+        $DRY_RUN_CMD mkdir -p $completion_path
       fi
 
-      $DRY_RUN_CMD ${pkgs.bat}/bin/bat --completion zsh > "$completion_folder/_bat"
-      $DRY_RUN_CMD ${pkgs.colima }/bin/colima completion zsh > "$completion_folder/_colima"
-      $DRY_RUN_CMD ${pkgs.dasel}/bin/dasel completion zsh >"$completion_folder/_dasel"
-      $DRY_RUN_CMD ${pkgs.docker}/bin/docker completion zsh > "$completion_folder/_docker"
+      $DRY_RUN_CMD ${pkgs.bat}/bin/bat --completion zsh > "$completion_path/_bat"
+      $DRY_RUN_CMD ${pkgs.colima }/bin/colima completion zsh > "$completion_path/_colima"
+      $DRY_RUN_CMD ${pkgs.dasel}/bin/dasel completion zsh >"$completion_path/_dasel"
+      $DRY_RUN_CMD ${pkgs.docker}/bin/docker completion zsh > "$completion_path/_docker"
       # ghostty completion comes bundle up with the application
-      $DRY_RUN_CMD ${pkgs.kubectl}/bin/kubectl completion zsh > "$completion_folder/_kubectl"
-      $DRY_RUN_CMD ${pkgs.ripgrep}/bin/rg --generate complete-zsh > "$completion_folder/_rg"
+      $DRY_RUN_CMD ${pkgs.kubectl}/bin/kubectl completion zsh > "$completion_path/_kubectl"
+      $DRY_RUN_CMD ${pkgs.ripgrep}/bin/rg --generate complete-zsh > "$completion_path/_rg"
 
-      $DRY_RUN_CMD ${pkgs.curl}/bin/curl --silent --show-error --output "$completion_folder/_delta" https://raw.githubusercontent.com/dandavison/delta/refs/heads/main/etc/completion/completion.zsh
-      $DRY_RUN_CMD ${pkgs.curl}/bin/curl --silent --show-error --output "$completion_folder/_eza" https://raw.githubusercontent.com/eza-community/eza/refs/heads/main/completions/zsh/_eza
-      $DRY_RUN_CMD ${pkgs.curl}/bin/curl --silent --show-error --output "$completion_folder/_fd" https://raw.githubusercontent.com/sharkdp/fd/refs/heads/master/contrib/completion/_fd
-      $DRY_RUN_CMD ${pkgs.curl}/bin/curl --silent --show-error --output "$completion_folder/_fzf" https://raw.githubusercontent.com/junegunn/fzf/refs/heads/master/shell/completion.zsh
-      $DRY_RUN_CMD ${pkgs.curl}/bin/curl --silent --show-error --output "$completion_folder/_git" https://raw.githubusercontent.com/git/git/refs/heads/master/contrib/completion/git-completion.zsh
-      $DRY_RUN_CMD ${pkgs.curl}/bin/curl --silent --show-error --output "$completion_folder/_home-manager" https://raw.githubusercontent.com/nix-community/home-manager/refs/heads/master/home-manager/completion.zsh
-      $DRY_RUN_CMD ${pkgs.curl}/bin/curl --silent --show-error --output "$completion_folder/_hurl" https://raw.githubusercontent.com/Orange-OpenSource/hurl/refs/heads/master/completions/_hurl
-      $DRY_RUN_CMD ${pkgs.curl}/bin/curl --silent --show-error --output "$completion_folder/_hurlfmt" https://raw.githubusercontent.com/Orange-OpenSource/hurl/refs/heads/master/completions/_hurlfmt
-      $DRY_RUN_CMD ${pkgs.curl}/bin/curl --silent --show-error --output "$completion_folder/_zoxide" https://raw.githubusercontent.com/ajeetdsouza/zoxide/refs/heads/main/contrib/completions/_zoxide
+      $DRY_RUN_CMD ${pkgs.curl}/bin/curl --silent --show-error --output "$completion_path/_delta" https://raw.githubusercontent.com/dandavison/delta/refs/heads/main/etc/completion/completion.zsh
+      $DRY_RUN_CMD ${pkgs.curl}/bin/curl --silent --show-error --output "$completion_path/_eza" https://raw.githubusercontent.com/eza-community/eza/refs/heads/main/completions/zsh/_eza
+      $DRY_RUN_CMD ${pkgs.curl}/bin/curl --silent --show-error --output "$completion_path/_fd" https://raw.githubusercontent.com/sharkdp/fd/refs/heads/master/contrib/completion/_fd
+      $DRY_RUN_CMD ${pkgs.curl}/bin/curl --silent --show-error --output "$completion_path/_fzf" https://raw.githubusercontent.com/junegunn/fzf/refs/heads/master/shell/completion.zsh
+      $DRY_RUN_CMD ${pkgs.curl}/bin/curl --silent --show-error --output "$completion_path/_git" https://raw.githubusercontent.com/git/git/refs/heads/master/contrib/completion/git-completion.zsh
+      $DRY_RUN_CMD ${pkgs.curl}/bin/curl --silent --show-error --output "$completion_path/_home-manager" https://raw.githubusercontent.com/nix-community/home-manager/refs/heads/master/home-manager/completion.zsh
+      $DRY_RUN_CMD ${pkgs.curl}/bin/curl --silent --show-error --output "$completion_path/_hurl" https://raw.githubusercontent.com/Orange-OpenSource/hurl/refs/heads/master/completions/_hurl
+      $DRY_RUN_CMD ${pkgs.curl}/bin/curl --silent --show-error --output "$completion_path/_hurlfmt" https://raw.githubusercontent.com/Orange-OpenSource/hurl/refs/heads/master/completions/_hurlfmt
+      $DRY_RUN_CMD ${pkgs.curl}/bin/curl --silent --show-error --output "$completion_path/_zoxide" https://raw.githubusercontent.com/ajeetdsouza/zoxide/refs/heads/main/contrib/completions/_zoxide
 
-      script_folder=${config.xdg.dataHome}/zsh/script
-      if [[ ! -d $script_folder ]]; then
-        $DRY_RUN_CMD mkdir -p $script_folder
+      script_path=${config.xdg.dataHome}/zsh/script
+      if [[ ! -d $script_path ]]; then
+        $DRY_RUN_CMD echo "Creating script folder at: $script_path"
+        $DRY_RUN_CMD mkdir -p $script_path
       fi
-      $DRY_RUN_CMD ${pkgs.curl}/bin/curl -o "$script_folder/git-completion.bash" https://raw.githubusercontent.com/git/git/refs/heads/master/contrib/completion/git-completion.bash
+
+      $DRY_RUN_CMD ${pkgs.curl}/bin/curl -o "$script_path/git-completion.bash" https://raw.githubusercontent.com/git/git/refs/heads/master/contrib/completion/git-completion.bash
     )
   '';
 }
