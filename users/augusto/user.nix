cd7744ca4b728e0cad49f53ef8b2325083fd1ab5
Augusto Melo <4723788+augustomelo@users.noreply.github.com>
2025-01-29 08:54:11 +0000

    docs: remove comment

--- users/augusto/user.nix
+++ users/augusto/user.nix
@@ -1,13 +1,5 @@
 { pkgs, ... }:
 {
-  # Create a user with my name
-  # Set the defalt shell, home and password
-  # https://github.com/mitchellh/nixos-config/blob/main/users/mitchellh/nixos.nix
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.mutableUsers = false;
-  # To generate a hashed password run mkpasswd
-  # https://search.nixos.org/options?channel=24.11&show=users.users.%3Cname%3E.hashedPassword&from=0&size=50&sort=relevance&type=packages&query=users.users
-
   programs.ssh.startAgent = true;
   programs.zsh.enable = true;
 
