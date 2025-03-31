efd3f1f55fb8318bc2c9243a456c10b899e460a0
Augusto Melo <4723788+augustomelo@users.noreply.github.com>
2025-03-31 20:14:17 +0100

    refactor: using mappings close to default

--- users/augusto/config/nvim/lua/autocmd.lua
+++ users/augusto/config/nvim/lua/autocmd.lua
@@ -15,12 +15,12 @@ vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(args)
     local default_opts = { noremap = true, silent = true, buffer = args.buf }
 
-    vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration,
+    vim.keymap.set("n", "grdc", vim.lsp.buf.declaration,
       { unpack(default_opts), desc = "Jumps to declaration." })
-    vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition,
+    vim.keymap.set("n", "grdf", vim.lsp.buf.definition,
       { unpack(default_opts), desc = "Jumps to definition." })
-    vim.keymap.set("n", "<leader>gt", vim.lsp.buf.type_definition,
-      { unpack(default_opts), desc = "Jumps to the definition." })
+    vim.keymap.set("n", "grtd", vim.lsp.buf.type_definition,
+      { unpack(default_opts), desc = "Jumps to the type definition." })
     vim.keymap.set("n", "<leader>ih",
       function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = args.buf, })) end,
       { unpack(default_opts), desc = "Toggle inlay hints" })
