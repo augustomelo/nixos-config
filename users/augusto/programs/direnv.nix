fea8c3d683b50b6fb0d4df77230d7be6b29e81d6
Augusto Melo <4723788+augustomelo@users.noreply.github.com>
2025-04-09 19:00:33 +0100

    feat: add whitelist direnv

--- users/augusto/programs/direnv.nix
+++ users/augusto/programs/direnv.nix
@@ -1,10 +1,15 @@
 {
-  pkgs,
   ...
 }:
 {
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
+    config = {
+      # https://direnv.net/man/direnv.toml.1.html#whitelist
+      whitelist = {
+        prefix = [ "~/workspace" ];
+      };
+    };
   };
 }
