162b8e609b80ef156f0403cbfb3f549c492f5398
Augusto Melo <4723788+augustomelo@users.noreply.github.com>
2025-08-28 14:00:55 +0100

    feat: add privoxy(gluetun) container

--- users/jb/containers/servarr.nix
+++ users/jb/containers/servarr.nix
@@ -5,8 +5,25 @@
     "d /home/jb/home-server/containers/servarr/storage/qbittorrent 0755 - - -"
   ];
 
-  services.podman.networks.servarr = {};
+  services.podman.networks.servarr = { };
   services.podman.containers = {
+    # https://github.com/qdm12/gluetun
+    # https://github.com/qdm12/gluetun-wiki
+    privoxy = {
+      image = "docker.io/qmcgaw/gluetun:v3.40";
+
+      environment = {
+        TZ = "Etc/UTC";
+        UPDATER_PERIOD = "24h";
+        VPN_SERVICE_PROVIDER = "mullvad";
+        VPN_TYPE = "wireguard";
+        WIREGUARD_ADDRESSES = "";
+        WIREGUARD_PRIVATE_KEY = "";
+      };
+      addCapabilities = [ "NET_ADMIN" ];
+      devices = [ "/dev/net/tun:/dev/net/tun" ];
+    };
+
     # https://docs.linuxserver.io/images/docker-qbittorrent
     qbittorrent = {
       image = "docker.io/linuxserver/qbittorrent:latest";
