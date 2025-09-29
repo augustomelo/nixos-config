8676313bd5f8ef0f827e07e085bcc6e95ccb47ca
Augusto Melo <4723788+augustomelo@users.noreply.github.com>
2025-09-29 09:02:57 +0100

    feat(devbox): move jujutsu config to its own file

--- users/augusto/programs/jujutsu.nix
+++ users/augusto/programs/jujutsu.nix
@@ -4,34 +4,5 @@
 {
   programs.jujutsu = {
     enable = true;
-    settings = {
-      git = {
-        private-commits = "description(glob:'private:*')";
-      };
-      user = {
-        email = "4723788+augustomelo@users.noreply.github.com";
-        name = "Augusto Melo";
-      };
-      ui.default-command = [ "log" ];
-      "--scope" = [
-        {
-          "--when"."commands" = [
-            "log"
-            "status"
-          ];
-          ui.paginate = "never";
-        }
-        {
-          "--when"."commands" = [
-            "diff"
-            "show"
-          ];
-          ui = {
-            pager = "delta";
-            diff-formatter = ":git";
-          };
-        }
-      ];
-    };
   };
 }
