e1a51b5960ba2ed717f3d0d814e202dfbaad0b66
Augusto Melo <4723788+augustomelo@users.noreply.github.com>
2025-10-05 19:30:50 +0100

    feat(home-server): immich use stable version

--- users/jb/containers/media.nix
+++ users/jb/containers/media.nix
@@ -16,18 +16,15 @@
   services.podman.containers = {
     # https://immich.app/docs/overview/welcome
     immich-server = {
-      image = "ghcr.io/immich-app/immich-server:v1.142.0";
+      image = "ghcr.io/immich-app/immich-server:v2.0.1";
 
       devices = [ "/dev/dri:/dev/dri" ];
-      environment = {
-        TZ = "Etc/UTC";
-        IMMICH_CONFIG_FILE = "/config";
-      };
+      environmentFile = [ "${config.sops.templates."containers/immich-server".path}" ];
       extraConfig = {
         Unit = {
-          After = "podman-immich-redis.service podman-immich-database.service";
-          Requires = "podman-immich-redis.service podman-immich-database.service";
-          PartOf = "podman-immich-redis.service podman-immich-database.service";
+          After = "podman-immich-cache.service podman-immich-database.service";
+          Requires = "podman-immich-cache.service podman-immich-database.service";
+          PartOf = "podman-immich-cache.service podman-immich-database.service";
         };
       };
       network = [
@@ -43,7 +40,7 @@
 
     # https://immich.app/docs/overview/welcome
     immich-machine-learning = {
-      image = "ghcr.io/immich-app/immich-machine-learning:v1.142.0";
+      image = "ghcr.io/immich-app/immich-machine-learning:v2.0.1";
 
       devices = [ "/dev/dri:/dev/dri" ];
       environment = {
@@ -59,8 +56,8 @@
     };
 
     # https://immich.app/docs/overview/welcome
-    immich-redis = {
-      image = "ghcr.io/valkey/valkey:8-bookworm@sha256:fea8b3e67b15729d4bb70589eb03367bab9ad1ee89c876f54327fc7c6e618571";
+    immich-cache = {
+      image = "docker.io/valkey/valkey:8-bookworm@sha256:fea8b3e67b15729d4bb70589eb03367bab9ad1ee89c876f54327fc7c6e618571";
 
       # Container.HealthCmd redis-cli ping || exit 1
 
@@ -86,19 +83,21 @@
     };
 
     # https://immich.app/docs/overview/welcome
-    immich-postgres = {
-      image = "ghcr.io/immich-app/postgres:14-vectorchord0.4.3-pgvectors0.2.0@sha256:8d292bdb796aa58bbbaa47fe971c8516f6f57d6a47e7172e62754feb6ed4e7b0";
+    immich-database = {
+      image = "ghcr.io/immich-app/postgres:14-vectorchord0.4.3-pgvectors0.2.0@sha256:41eacbe83eca995561fe43814fd4891e16e39632806253848efaf04d3c8a8b84";
 
       devices = [ "/dev/dri:/dev/dri" ];
       environmentFile = [ "${config.sops.templates."containers/immich-database".path}" ];
       extraConfig = {
         Container = {
-          ShmSize = "128md";
+          ShmSize = "128mb";
         };
       };
       network = [
         "media"
       ];
+      user = "jb";
+      userNS = "keep-id";
       volumes = [
         "/home/jb/home-server/containers/media/storage/immich-database:/var/lib/postgresql/data"
       ];
@@ -132,12 +131,19 @@
       "containers/immich/postgres_password" = { };
     };
     templates = {
+      "containers/immich-server".content = ''
+        TZ=Etc/UTC
+        DB_HOSTNAME=immich-database
+        DB_PASSWORD=${config.sops.placeholder."containers/immich/postgres_password"}
+        REDIS_HOSTNAME=immich-cache
+      '';
+
       "containers/immich-database".content = ''
-        TZ=Etc/UTC;
-        POSTGRES_PASSWORD=${config.sops.placeholder."containers/immich/postgres_password"};
-        POSTGRES_USER=postgres;
-        POSTGRES_DB=immich;
-        POSTGRES_INITDB_ARGS=--data-checksums;
+        TZ=Etc/UTC
+        POSTGRES_PASSWORD=${config.sops.placeholder."containers/immich/postgres_password"}
+        POSTGRES_USER=postgres
+        POSTGRES_DB=immich
+        POSTGRES_INITDB_ARGS=--data-checksums
       '';
     };
   };
