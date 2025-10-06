4f1545a573590fb8bcbe81a11b9086356fa52941
Augusto Melo <4723788+augustomelo@users.noreply.github.com>
2025-10-06 16:37:39 +0100

    feat(devbox): fix podman rootless mode

--- users/augusto/user.nix
+++ users/augusto/user.nix
@@ -12,6 +12,19 @@
       shell = pkgs.bash;
       extraGroups = [ "wheel" ];
       hashedPassword = "$y$j9T$5qUofB6UibbNyT6gQ7nDX/$QpaTGYmim85ItVepaLalPmtPg1D/A6eFJj6YsCWMQfB";
+      # https://docs.podman.io/en/latest/markdown/podman.1.html#rootless-mode
+      subUidRanges = [
+        {
+          startUid = 10000;
+          count = 75535;
+        }
+      ];
+      subGidRanges = [
+        {
+          startGid = 10000;
+          count = 75535;
+        }
+      ];
     };
   };
 }
