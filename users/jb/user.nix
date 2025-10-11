35da82bc02c126cb9c198682ca4c1a4fea6c1615
Augusto Melo <4723788+augustomelo@users.noreply.github.com>
2025-10-11 15:54:12 +0100

    feat(home-server): activating podman using `virtualisation.podman.enable`
    This also anable other servies that are necessary, and for example when
    using nftables

--- users/jb/user.nix
+++ users/jb/user.nix
@@ -11,21 +11,30 @@
     ssh.startAgent = true;
   };
 
-  networking.firewall = {
-    enable = true;
-    allowedTCPPorts = [
-      2283
-      5055
-      6767
-      7878
-      8000
-      8080
-      8888
-      8989
-      9696
-    ];
+  virtualisation = {
+    containers.enable = true;
+    podman = {
+      enable = true;
+    };
   };
 
+  networking = {
+    nftables.enable = true;
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [
+        2283
+        5055
+        6767
+        7878
+        8000
+        8080
+        8888
+        8989
+        9696
+      ];
+    };
+  };
 
   # Needed by podman-user-wait-network-online.service
   # https://github.com/containers/podman/issues/24796
@@ -56,7 +65,10 @@
       linger = true;
       isNormalUser = true;
       shell = pkgs.bash;
-      extraGroups = [ "wheel" ];
+      extraGroups = [
+        "podman"
+        "wheel"
+      ];
       hashedPasswordFile = "/etc/nixos/passwd";
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEGL2rZ4htp01YDa9Q6iqwcmtpkawn6VdCu25idKDHKm jb@home-server"
