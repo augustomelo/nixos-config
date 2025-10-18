86625746f52e3b70a805ebfd7a1624c84566e7af
Augusto Melo <4723788+augustomelo@users.noreply.github.com>
2025-10-18 14:50:32 +0100

    feat(home-server, network): add proxy

--- users/jb/containers/network.nix
+++ users/jb/containers/network.nix
@@ -31,11 +31,25 @@
         "/home/jb/home-server/containers/network/config/pihole:/etc/pihole"
       ];
     };
+
+    # https://github.com/qdm12/gluetun
+    # https://github.com/qdm12/gluetun-wiki
+    proxy = {
+      image = "docker.io/qmcgaw/gluetun:v3.40";
+
+      addCapabilities = [ "NET_ADMIN" ];
+      devices = [ "/dev/net/tun:/dev/net/tun" ];
+      environmentFile = [ "${config.sops.templates."containers/proxy".path}" ];
+      network = [ "network" ];
+      ports = [ "9999:8888" ];
+    };
   };
 
   sops = {
     secrets = {
       "containers/pihole/webserver_api_password" = { };
+      "containers/proxy/wireguard_addresses" = { };
+      "containers/proxy/wireguard_private_key" = { };
     };
     templates = {
       "containers/pihole".content = ''
@@ -45,6 +59,16 @@
         PIHOLE_GID=100
         TZ=UTC
         WEBTHEME=default-dark
+      '';
+      "containers/proxy".content = ''
+        HTTPPROXY=on
+        SERVER_COUNTRIES=Brazil
+        TZ=Etc/UTC
+        UPDATER_PERIOD=24h
+        VPN_SERVICE_PROVIDER=mullvad
+        VPN_TYPE=wireguard
+        WIREGUARD_ADDRESSES=${config.sops.placeholder."containers/proxy/wireguard_addresses"}
+        WIREGUARD_PRIVATE_KEY=${config.sops.placeholder."containers/proxy/wireguard_private_key"}
       '';
     };
   };
