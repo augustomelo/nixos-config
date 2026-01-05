96779f1591d6ece8f6acb1673dff32d9f39903b7
Augusto Melo <4723788+augustomelo@users.noreply.github.com>
2026-01-05 17:21:40 +0000

    feat(home-server,qbittorrent): add healthcheck

--- users/augusto/modules/containers/servarr/qbittorrent.nix
+++ users/augusto/modules/containers/servarr/qbittorrent.nix
@@ -17,7 +17,7 @@ in
 
     # https://docs.linuxserver.io/images/docker-qbittorrent
     services.podman.containers.qbittorrent = {
-      image = "docker.io/linuxserver/qbittorrent:libtorrentv1";
+      image = "docker.io/linuxserver/qbittorrent:latest";
 
       autoUpdate = "registry";
       environment = {
@@ -28,6 +28,14 @@ in
           After = "podman-privoxy.service";
           Requires = "podman-privoxy.service";
           PartOf = "podman-privoxy.service";
+        };
+        X-Container = {
+          HealthCmd = "curl -s http://localhost:8080/api/v2/transfer/info | jq -e '.connection_status == \"connected\" or .connection_status == \"firewalled\"' || exit 1";
+          HealthInterval = "30s";
+          HealthOnFailure = "stop";
+          HealthRetries = "3";
+          HealthStartPeriod = "60s";
+          HealthTimeout = "10s";
         };
       };
       network = [ "container:privoxy" ];
