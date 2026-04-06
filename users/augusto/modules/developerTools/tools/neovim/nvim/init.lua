88529acd65d840df7a0a021e3d932f2a95ef3e60
Augusto Melo <4723788+augustomelo@users.noreply.github.com>
2026-04-06 09:37:36 +0100

    refactor(devbox,neovim): move global behavior to init.lua

--- users/augusto/modules/developerTools/tools/neovim/nvim/init.lua
+++ users/augusto/modules/developerTools/tools/neovim/nvim/init.lua
@@ -1,1 +1,13 @@
+require('vim._core.ui2').enable({}) -- experimental
+
 vim.cmd.packadd("nvim.undotree")
+
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.mapleader = ","
+vim.g.netrw_banner = 0
+vim.g.netrw_liststyle = 3
+
+vim.loader.enable() -- experimental
