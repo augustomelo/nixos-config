3da13ad578298cd52d43b0bbac4b4df9ed02de39
Augusto Melo <4723788+augustomelo@users.noreply.github.com>
2026-05-05 20:31:46 +0100

    feat(home-server,copyparty): custom docker image with ffmpeg

--- users/augusto/modules/containers/nas/copyparty.nix
+++ users/augusto/modules/containers/nas/copyparty.nix
@@ -1,6 +1,7 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }:
 let
@@ -13,11 +14,39 @@ in
       "d ${shareFolder} 0755 - - -"
     ];
 
+    # Enable hevc support
+    # https://github.com/9001/copyparty/blob/hovudstraum/docs/bad-codecs.md
+    services.podman.builds = {
+      "copyparty" = {
+        file =
+          let
+            containerFile = pkgs.writeTextFile {
+              name = "Containerfile";
+              text = ''
+                FROM docker.io/copyparty/ac:latest
+                RUN apk del ffmpeg \
+                  ffmpeg-libavcodec \
+                  ffmpeg-libavdevice \
+                  ffmpeg-libavfilter \
+                  ffmpeg-libavformat \
+                  ffmpeg-libavutil \
+                  ffmpeg-libs \
+                  ffmpeg-libswresample \
+                  ffmpeg-libswscale \
+                  && apk add --no-cache py3-pip ffmpeg vips vips-jxl vips-heif vips-poppler vips-magick \
+                  && rm -f /usr/lib/python3*/EXTERNALLY-MANAGED \
+                  && python3 -m pip install pyvips
+              '';
+            };
+          in
+          "${containerFile}";
+      };
+    };
+
     # https://github.com/9001/copyparty
     services.podman.containers.copyparty = {
-      image = "docker.io/copyparty/iv:latest";
+      image = "copyparty.build";
 
-      autoUpdate = "registry";
       network = [
         "nas"
       ];
