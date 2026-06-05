13a10947ee321490b954f7f38f1680c42a6b5c88
Augusto Melo <4723788+augustomelo@users.noreply.github.com>
2026-06-05 17:30:16 +0100

    feat(devbox): address obsidian deprecation

--- users/augusto/modules/developerTools/tools/neovim/nvim/after/plugin/obsidian.lua
+++ users/augusto/modules/developerTools/tools/neovim/nvim/after/plugin/obsidian.lua
@@ -1,10 +1,6 @@
 -- https://github.com/obsidian-nvim/obsidian.nvim
 
 require("obsidian").setup({
-  completion = {
-    nvim_cmp = false,
-    blink = true,
-  },
   legacy_commands = false,
   workspaces = {
     {
